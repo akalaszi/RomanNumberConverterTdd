0f6d92a4dc6f445a1461f2477a800fa87e063e02
Adrian Kalaszi <kalaszi.adrian@tech.jago.com>
2024-03-17 08:06:22 +0100

    Green: III

--- src/main/kotlin/RomanNumberConverter.kt
+++ src/main/kotlin/RomanNumberConverter.kt
@@ -3,6 +3,7 @@ package org.example
 class RomanNumberConverter {
     companion object {
         fun convert(arabic: Int): String {
+            if (arabic == 3) return "III"
             if (arabic == 2) return "II"
             return "I"
         }
