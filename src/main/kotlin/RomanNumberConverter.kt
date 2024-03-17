43c5bf2671ae22bf092b5976c6056751953688b0
Adrian Kalaszi <kalaszi.adrian@tech.jago.com>
2024-03-17 08:32:14 +0100

    Green: 5,6,8

--- src/main/kotlin/RomanNumberConverter.kt
+++ src/main/kotlin/RomanNumberConverter.kt
@@ -8,10 +8,16 @@ class RomanNumberConverter {
                 for (i in 1..arabic) {
                     ret += "I"
                 }
-            } else if(arabic==4) {
+            } else if (arabic == 4) {
                 ret = "IV"
-            } else if(arabic==5) {
+            } else if (arabic == 5) {
                 ret = "V"
+            } else if (arabic == 6) {
+                ret = "VI"
+            } else if (arabic == 7) {
+                ret = "VII"
+            } else if (arabic == 8) {
+                ret = "VIII"
             }
 
             return ret
