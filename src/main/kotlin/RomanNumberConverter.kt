26dbe19e0f618380bc4d4c07e58536688a80d0bd
Adrian Kalaszi <kalaszi.adrian@tech.jago.com>
2024-03-17 08:21:17 +0100

    Green: 4,5

--- src/main/kotlin/RomanNumberConverter.kt
+++ src/main/kotlin/RomanNumberConverter.kt
@@ -4,9 +4,16 @@ class RomanNumberConverter {
     companion object {
         fun convert(arabic: Int): String {
             var ret = ""
-            for (i in 1..arabic) {
-                ret += "I"
+            if (arabic < 4) {
+                for (i in 1..arabic) {
+                    ret += "I"
+                }
+            } else if(arabic==4) {
+                ret = "IV"
+            } else if(arabic==5) {
+                ret = "V"
             }
+
             return ret
         }
     }
