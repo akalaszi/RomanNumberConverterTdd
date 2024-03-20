599a387b2d1b67b37766cfe783c2db3e409331a4
Adrian Kalaszi <kalaszi.adrian@tech.jago.com>
2024-03-20 07:04:33 +0100

    Green C

--- src/main/kotlin/RomanNumberConverter.kt
+++ src/main/kotlin/RomanNumberConverter.kt
@@ -13,7 +13,7 @@ class RomanNumberConverter {
             return digits
         }
 
-        private fun process(number: Int, numerals:Numerals ): String {
+        private fun process(number: Int, numerals: Numerals): String {
             var a = number
             var ret = ""
             if (a % 5 == 4) {
@@ -40,14 +40,19 @@ class RomanNumberConverter {
         fun convert(arabic: Int): String {
             val ones = Numerals("I", "V", "X")
             val tens = Numerals("X", "L", "C")
+            val hundreds = Numerals("C", "D", "M")
             val digits = splitToDigits(arabic)
             var ret = ""
+            if (digits.size > 2) {
+                ret += process(digits[2], hundreds)
+            }
             if (digits.size > 1) {
                 ret += process(digits[1], tens)
             }
             ret += process(digits[0], ones)
             return ret
         }
-        data class Numerals(val one:String, val five:String, val ten:String)
+
+        data class Numerals(val one: String, val five: String, val ten: String)
     }
 }
